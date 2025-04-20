367c7ff4ffc301d9aa16efb110d5f62e1e5a7760
cachi1997 <cachigaliano1997@gmail.com>
2025-04-19 23:13:15 -0300

    feat: initialize React tip calculator app with Vite and TypeScript
    - Add package.json with dependencies and scripts for development and build
    - Create main application component (App.tsx) with basic layout
    - Set up Tailwind CSS for styling in index.css
    - Implement main entry point (main.tsx) for rendering the app
    - Configure TypeScript with tsconfig files for app and node
    - Add Vite configuration for React plugin

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { menuItems } from "./data/db";
+
+function App() {
+  console.log(menuItems);
+
+  return (
+    <>
+      <header className="bg-teal-400 py-5">
+        <h1 className="text-center text-4xl font-black">
+          Calculadora de propinas
+        </h1>
+      </header>
+      <main className="py-20 max-w-7xl mx-auto grid md:grid-cols-2">
+        <div>
+          <h2>Menú</h2>
+        </div>
+        <div>
+          <h2>Consumo</h2>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
